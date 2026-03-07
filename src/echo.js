812d48e2df3253ee550b0cdddcd3f39b90296ac9
GabrielMendes <imgabrielmendes1@gmail.com>
2026-03-07 09:15:53 -0300

    [FEAT] - Comentei o pusher e o echo

--- src/echo.js
+++ src/echo.js
@@ -1,15 +1,15 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    forceTLS: false, // Obrigatório para HTTP (Soketi local)
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-    cluster: 'mt1', // ADICIONE ISSO AQUI (pode ser qualquer string)
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     wsHost: import.meta.env.VITE_PUSHER_HOST,
+//     wsPort: import.meta.env.VITE_PUSHER_PORT,
+//     forceTLS: false, // Obrigatório para HTTP (Soketi local)
+//     disableStats: true,
+//     enabledTransports: ['ws', 'wss'],
+//     cluster: 'mt1', // ADICIONE ISSO AQUI (pode ser qualquer string)
+// });
